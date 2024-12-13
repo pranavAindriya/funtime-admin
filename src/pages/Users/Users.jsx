06492e87a40b3e18c250d72fc0ce1faafee37ab3
Pranav-pradeep0 <pranavpradeep1120@gmail.com>
2024-12-13 17:58:18 +0530

    users field navigation persist integrations

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,42 +1,54 @@
 import { TabContext, TabList, TabPanel } from "@mui/lab";
 import { Box, Button, Tab, useTheme } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import Userlist from "./UserTabs/Userlist";
 import KycRequests from "./UserTabs/KycRequests";
 import BlackLists from "./UserTabs/BlackLists";
-import { Plus } from "@phosphor-icons/react";
-import { useNavigate } from "react-router-dom";
 import HostedUsers from "./UserTabs/HostedUsers";
+import { Plus } from "@phosphor-icons/react";
 
 const tabs = [
   {
     label: "User List",
+    value: "users",
     component: <Userlist />,
   },
   {
     label: "Host Requests",
+    value: "hosts",
     component: <HostedUsers />,
   },
   {
     label: "KYC Requests",
+    value: "kyc",
     component: <KycRequests />,
   },
   {
     label: "Black List",
+    value: "blacklist",
     component: <BlackLists />,
   },
 ];
 
 const Users = () => {
   const theme = useTheme();
-
   const navigate = useNavigate();
+  const location = useLocation();
 
-  const [value, setValue] = useState("1");
+  const searchParams = new URLSearchParams(location.search);
+  const defaultTab = searchParams.get("tab") || "users";
+
+  const [value, setValue] = useState(defaultTab);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
+    navigate(`?tab=${newValue}`);
   };
+
+  useEffect(() => {
+    setValue(defaultTab);
+  }, [defaultTab]);
 
   return (
     <Box sx={{ width: "100%", typography: "body1" }}>
@@ -55,19 +67,19 @@ const Users = () => {
       <TabContext value={value}>
         <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
           <TabList onChange={handleChange} aria-label="lab API tabs example">
-            {tabs.map((tab, ind) => (
+            {tabs.map((tab) => (
               <Tab
-                key={ind}
+                key={tab.value}
                 label={tab.label}
-                value={(ind + 1).toString()}
+                value={tab.value}
                 disableRipple
               />
             ))}
           </TabList>
         </Box>
-        {tabs.map((tab, ind) => (
-          <TabPanel key={ind} value={(ind + 1).toString()}>
-            {tab?.component}
+        {tabs.map((tab) => (
+          <TabPanel key={tab.value} value={tab.value}>
+            {tab.component}
           </TabPanel>
         ))}
       </TabContext>
